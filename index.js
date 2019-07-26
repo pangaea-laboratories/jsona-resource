bc34d5fffc3f6e013c151f8f6b91f1a14d24d344
Daniel Yovchev <daniel@yovchev.io>
2019-07-26 15:36:07 +0100

    Allow access for resource/relationship

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Jsona from 'jsona'
 import pluralize from 'pluralize'
-import { merge, camelCase } from 'lodash'
+import { merge, camelCase, isObject, isString, isFunction } from 'lodash'
 
 export const methods = [
     {
@@ -32,21 +32,25 @@ export const methods = [
 
 const dataFormatter = new Jsona()
 
-const endpoint = (resource, method, httpService) => {
-    return function(params = {}, callback) {
-        if(params.include instanceof Array) {
-            params.include = params.include.join(',')
-        }
-        return new Promise((resolve, reject) => {
-            httpService[`${method.method.toLowerCase()}`](resource.name, { params })
-            .then((response) => {
-                resolve(dataFormatter.deserialize(response.data))
-                if(typeof callback === 'function') {
-                    callback(response, dataFormatter)
-                }
-            })
-            .catch((error) => reject(error))
-        })
+const parseParams = (params) => {
+    if(params.include instanceof Array) {
+        params.include = params.include.join(',')
+    }
+
+    return params
+}
+
+const errorFunction = (error, callback, reject) => {
+    reject(error)
+    if(isFunction(callback)) {
+        callback(error)
+    }
+}
+
+const responseFunction = (response, callback, resolve) => {
+    resolve(dataFormatter.deserialize(response.data))
+    if(isFunction(callback)) {
+        callback(response, dataFormatter)
     }
 }
 
@@ -54,9 +58,31 @@ export const JsonApiResource = function(resource, httpService) {
     let endpoints = {}
     methods.forEach((method) => {
         if(method.pluralize) {
-            endpoints[camelCase(`${method.name}-${pluralize(resource.name)}`)] = endpoint(resource, method, httpService)
+            endpoints[camelCase(`${method.name}-${pluralize(resource.name)}`)] = function(params = {}, callback) {
+                return new Promise((resolve, reject) => {
+                    httpService[`${method.method.toLowerCase()}`](resource.name, { params: parseParams(params) })
+                    .then((response) => responseFunction(response, callback, resolve))
+                    .catch((error) => errorFunction(error, callback, reject))
+                })
+            }
         } else {
-            endpoints[camelCase(`${method.name}-${pluralize.singular(resource.name)}`)] = endpoint(resource, method, httpService)
+            // api.fetchAccount(1, {}) // Fech account with id 1 and params
+            // api.fetchAccount({}, {}) // Fech account.id with params
+            // api.fetchAccount({}, '', {}) // Fetch account.id appendUrl and parms
+            // api.fetchAccount(accounts[0], 'accountProducts')
+            endpoints[camelCase(`${method.name}-${pluralize.singular(resource.name)}`)] = function() {
+                // we have the full resource
+                let id = isObject(arguments[0]) ? arguments[0].id : arguments[0]
+                let uri = isString(arguments[1]) ? `/${arguments[1]}` : ''
+                let params = isObject(arguments[1]) ? arguments[1] : arguments[2] || {}
+                let callback = isFunction(arguments[2]) ? arguments[2] : arguments[3]
+
+                return new Promise((resolve, reject) => {
+                    httpService[`${method.method.toLowerCase()}`](`${resource.name}/${id}${uri}`, { params: parseParams(params) })
+                    .then((response) => responseFunction(response, callback, resolve))
+                    .catch((error) => errorFunction(error, callback, reject))
+                })
+            }
         }
     })
     return endpoints
